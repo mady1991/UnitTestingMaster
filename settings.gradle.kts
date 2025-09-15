662bbde63be93d78a28505ba79b541d4d9bc4c38
mady1991 <53367121+mady1991@users.noreply.github.com>
2025-09-15 19:35:56 +0400

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "android_unit_testing_master"
+include(":app")
+include(":basic_test")
+include(":unit_test_in_android")
